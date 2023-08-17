e42f62f027b1e9ad6b468b4ba0a6caaa73726a5e
Kostiantyn Popravka <popravka1209@gmail.com>
2023-08-17 19:02:08 +0200

    deleted "scrape ottomoto" it crashed whole app

--- src/App.js
+++ src/App.js
@@ -6,15 +6,13 @@ import CarList from './components/CarList';
 import styles from './appWrapper.module.css'
 import Footer from './components/Footer';
 import './index.css';
-import storeParse from "./redux/storeParse";
 
 const App = () => {
     return (
-        <Provider store={storeParse}>
+        <Provider store={store}>
             <div className={styles.navbar}>
                 <Navbar />
             </div>
-                {/* Conditional rendering for CarList or CarDetails based on navigation */}
             <div className={styles.content}>
                 <CarList />
             </div>
