a21870fccfb7dc756b770c3ff88fb16b120770a0
Nikita <hailrake04@gmail.com>
2023-08-23 17:46:07 -0400

    header and footer with different color than body when turning the dark theme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,9 +7,18 @@ const Navbar = () => {
   const toggleTheme = () => {
     setTheme(!isDarkTheme);
 
+    // assigns dark theme to all children in root div
     document.getElementById("root").childNodes.forEach((e) => {
       e.classList.toggle(darkThemeStyles["dark-theme"]);
     });
+
+    // get all elemets in root div whoose class does not include word "content"
+    let nodes = document.getElementById("root").childNodes;
+    for (let i = 0; i < nodes.length; i++) {
+      if (!nodes[i].className.includes("content")) {
+        nodes[i].classList.toggle(darkThemeStyles["light-theme"]);
+      }
+    }
   };
 
   return (
